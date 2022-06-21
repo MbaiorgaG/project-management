825f7160d150fdc52f1d1588a8119ade625874c9
Gabriel Simon Mbaiorga <gabrielmbaiorga@gmail.com>
2022-06-21 22:50:22 +0100

    Added checkedField Dto

---
+++ src/main/java/com/example/projectmanagement/dto/utils/CheckFieldAndValuesDTO.java
@@ -0,0 +1,22 @@
+package com.example.projectmanagement.dto.utils;
+
+public class CheckFieldAndValuesDTO {
+    private String orderedFieldValues;
+    private String orderedFieldNames;
+
+    public String getOrderedFieldValues() {
+        return orderedFieldValues;
+    }
+
+    public void setOrderedFieldValues(String orderedFieldValues) {
+        this.orderedFieldValues = orderedFieldValues;
+    }
+
+    public String getOrderedFieldNames() {
+        return orderedFieldNames;
+    }
+
+    public void setOrderedFieldNames(String orderedFieldNames) {
+        this.orderedFieldNames = orderedFieldNames;
+    }
+}
