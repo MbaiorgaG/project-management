e2a974281bffbe03ccd86843ec7dabb6777ec5a7
Gabriel Simon Mbaiorga <gabrielmbaiorga@gmail.com>
2022-06-22 23:53:51 +0100

    Added unit test for Decrypt class

---
+++ src/test/java/com/example/projectmanagement/utility/DecryptTest.java
@@ -0,0 +1,34 @@
+package com.example.projectmanagement.utility;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DecryptTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void decryptRequestParam() {
+    }
+
+    @Test
+    void validatedCheckSum() {
+    }
+
+    @Test
+    void validateCheckSum() {
+    }
+
+    @Test
+    void decryptData() {
+    }
+}
