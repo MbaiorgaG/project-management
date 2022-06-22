7809cb77e5fab8e51c45e27e317c70f13f61edd4
Gabriel Simon Mbaiorga <gabrielmbaiorga@gmail.com>
2022-06-22 23:50:10 +0100

    Added a date Util to manage all dates in the application.

---
+++ src/main/java/com/example/projectmanagement/utility/DateUtil.java
@@ -0,0 +1,9 @@
+package com.example.projectmanagement.utility;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DateUtil {
+
+    static final Logger logger = LoggerFactory.getLogger(DateUtil.class);
+}
