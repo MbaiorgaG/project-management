7f69506ae14fccb6ed253da31bee4c4b388c3661
Gabriel Simon Mbaiorga <gabrielmbaiorga@gmail.com>
2022-06-23 22:27:43 +0100

    Added date util test

---
+++ src/test/java/com/example/projectmanagement/utility/DateUtilTest.java
@@ -0,0 +1,79 @@
+package com.example.projectmanagement.utility;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DateUtilTest {
+
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void validate() {
+        assertEquals("test", "test");
+    }
+
+    @Test
+    void convertDateToLong() {
+        assertEquals("test", "test");
+    }
+
+    @Test
+    void nextDate() {
+        assertEquals("test", "test");
+    }
+
+    @Test
+    void isSameDayOfTheYear() {
+        assertEquals("test", "test");
+    }
+
+    @Test
+    void isWithinCurrentDateRange() {
+        assertEquals("test", "test");
+    }
+
+    @Test
+    void getToday() {
+        assertEquals("test", "test");
+    }
+
+    @Test
+    void getEndOfDay() {
+        assertEquals("test", "test");
+    }
+
+    @Test
+    void getStartOfDay() {
+        assertEquals("test", "test");
+    }
+
+    @Test
+    void convertStringToDate() {
+        assertEquals("test", "test");
+    }
+
+    @Test
+    void addDaysToDate() {
+        assertEquals("test", "test");
+    }
+
+    @Test
+    void isBeforeDay() {
+        assertEquals("test", "test");
+    }
+
+    @Test
+    void testIsBeforeDay() {
+        assertEquals("test", "test");
+    }
+}
